6d554431c218d9aeecca895a530ba058a8d5ef9e
AnjuCsuresh <anjuc96@gmail.com>
2013-12-03 16:56:44 +0530

    managed running and stopping  project and spider

--- spider/angular/app.js
+++ spider/angular/app.js
@@ -1,35 +1,192 @@
 'use strict';
 
-
+function log(data){
+    console.log(JSON.stringify(data))
+}
 // Declare app level module which depends on filters, and services
-angular.module('myApp', ['myApp.controllers', 'ngCookies']).
-    config(['$routeProvider', function ($routeProvider) {
-        $routeProvider.when('/', {templateUrl: 'templates/login.html', controller: 'LoginCtrl'});
-        $routeProvider.when('/signup', {templateUrl: 'templates/signup.html', controller: 'RegisterCtrl'});
-        $routeProvider.when('/forgot', {templateUrl: 'templates/forgot.html', controller: 'ForgotCtrl'});
-        $routeProvider.when('/dashboard', {templateUrl: 'templates/dashboard.html', controller: 'DashboardCtrl'});
-        $routeProvider.when('/docs', {templateUrl: 'templates/documentation.html', controller: 'DocsCtrl'});
-        $routeProvider.when('/setting', {templateUrl: 'templates/setting.html', controller: 'SettingCtrl'});
-        $routeProvider.when('/activity', {templateUrl: 'templates/activitylog.html', controller: 'ActivityCtrl'});
-        $routeProvider.when('/schedule', {templateUrl: 'templates/schedule.html', controller: 'ScheduleCtrl'}); 
-        $routeProvider.when('/project/:current_project', {templateUrl: 'templates/project.html', controller: 'ProjectCtrl'});       
-        $routeProvider.otherwise({redirectTo: '/'});
-    }])
+angular.module('myApp', ['myApp.controllers','myApp.services', 'ngCookies', 'notifications']).
+config(['$routeProvider',
+    function($routeProvider) {
+        $routeProvider.when('/', {
+            templateUrl: 'templates/login.html',
+            controller: 'LoginCtrl'
+        });
+        $routeProvider.when('/signup', {
+            templateUrl: 'templates/signup.html',
+            controller: 'RegisterCtrl'
+        });
+        $routeProvider.when('/forgot', {
+            templateUrl: 'templates/forgot.html',
+            controller: 'ForgotCtrl'
+        });
+        $routeProvider.when('/dashboard', {
+            templateUrl: 'templates/dashboard.html',
+            controller: 'DashboardCtrl'
+        });
+        $routeProvider.when('/docs', {
+            templateUrl: 'templates/documentation.html',
+            controller: 'DocsCtrl'
+        });
+        $routeProvider.when('/setting', {
+            templateUrl: 'templates/setting.html',
+            controller: 'SettingCtrl'
+        });
+        $routeProvider.when('/activity', {
+            templateUrl: 'templates/activitylog.html',
+            controller: 'ActivityCtrl'
+        });
+        $routeProvider.when('/schedule', {
+            templateUrl: 'templates/schedule.html',
+            controller: 'ScheduleCtrl'
+        });
+        $routeProvider.when('/project', {
+            templateUrl: 'templates/project.html',
+            controller: 'ProjectCtrl'
+        });
+        $routeProvider.when('/prjctlist', {
+            templateUrl: 'templates/proList.html',
+            controller: 'ProjectListCtrl'
+        });
+        $routeProvider.when('/plans', {
+            templateUrl: 'templates/plans.html',
+            controller: 'PlanCtrl'
+        });
+        $routeProvider.when('/payment', {
+            templateUrl: 'templates/payment.html',
+            controller: 'PayCtrl'
+        });
+        $routeProvider.otherwise({
+            redirectTo: '/'
+        });
+    }
+])
 
-    .config(['$httpProvider', function ($httpProvider) {
+.config(['$httpProvider',
+    function($httpProvider) {
         $httpProvider.defaults.useXDomain = true;
         delete $httpProvider.defaults.headers.common['X-Requested-With'];
         $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
     }
-    ])
+])
+.config(function($httpProvider) {
+        var numLoadings = 0;
+        var loadingScreen = $('<div style="position:fixed;top:0;left:0;right:0;bottom:0;z-index:10000;background-color:white;background-color:rgba(255,255,255,0.6);"><div style="position:absolute;top:45%;left:45.5%;font-size:4em;text-align:center"><img src="img/ajax-loader.gif" alt="" /></div></div>')
+            .appendTo($('body')).hide();
+       $httpProvider.responseInterceptors.push(function() {
+            return function(promise) {
+                numLoadings++;
+                loadingScreen.show();
+                var hide = function(r) { if (!(--numLoadings)) loadingScreen.hide(); return r; };
+                return promise.then(hide, hide);
+            };
+        });
+    })
+.factory('Base64', function () {
+        var keyStr = 'ABCDEFGHIJKLMNOP' +
+            'QRSTUVWXYZabcdef' +
+            'ghijklmnopqrstuv' +
+            'wxyz0123456789+/' +
+            '=';
+        return {
+            encode: function (input) {
+                var output = "";
+                var chr1, chr2, chr3 = "";
+                var enc1, enc2, enc3, enc4 = "";
+                var i = 0;
 
-    .run(function ($rootScope, $location, $anchorScroll, $routeParams,$window,$cookies) {
+                do {
+                    chr1 = input.charCodeAt(i++);
+                    chr2 = input.charCodeAt(i++);
+                    chr3 = input.charCodeAt(i++);
 
-        $rootScope.$on('$routeChangeSuccess', function (newRoute, oldRoute) {
-                $location.hash($routeParams.scrollTo);
-                $anchorScroll();
+                    enc1 = chr1 >> 2;
+                    enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+                    enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+                    enc4 = chr3 & 63;
+
+                    if (isNaN(chr2)) {
+                        enc3 = enc4 = 64;
+                    } else if (isNaN(chr3)) {
+                        enc4 = 64;
+                    }
+
+                    output = output +
+                        keyStr.charAt(enc1) +
+                        keyStr.charAt(enc2) +
+                        keyStr.charAt(enc3) +
+                        keyStr.charAt(enc4);
+                    chr1 = chr2 = chr3 = "";
+                    enc1 = enc2 = enc3 = enc4 = "";
+                } while (i < input.length);
+
+                return output;
             },
-            $rootScope.location = $location
-        );
-    });
 
+            decode: function (input) {
+                var output = "";
+                var chr1, chr2, chr3 = "";
+                var enc1, enc2, enc3, enc4 = "";
+                var i = 0;
+
+                // remove all characters that are not A-Z, a-z, 0-9, +, /, or =
+                var base64test = /[^A-Za-z0-9\+\/\=]/g;
+                if (base64test.exec(input)) {
+                    alert("There were invalid base64 characters in the input text.\n" +
+                        "Valid base64 characters are A-Z, a-z, 0-9, '+', '/',and '='\n" +
+                        "Expect errors in decoding.");
+                }
+                input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+
+                do {
+                    enc1 = keyStr.indexOf(input.charAt(i++));
+                    enc2 = keyStr.indexOf(input.charAt(i++));
+                    enc3 = keyStr.indexOf(input.charAt(i++));
+                    enc4 = keyStr.indexOf(input.charAt(i++));
+
+                    chr1 = (enc1 << 2) | (enc2 >> 4);
+                    chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+                    chr3 = ((enc3 & 3) << 6) | enc4;
+
+                    output = output + String.fromCharCode(chr1);
+
+                    if (enc3 != 64) {
+                        output = output + String.fromCharCode(chr2);
+                    }
+                    if (enc4 != 64) {
+                        output = output + String.fromCharCode(chr3);
+                    }
+
+                    chr1 = chr2 = chr3 = "";
+                    enc1 = enc2 = enc3 = enc4 = "";
+
+                } while (i < input.length);
+
+                return output;
+            }
+        };
+    }).factory('Auth', function (Base64, $cookieStore, $http) {
+    // initialize to whatever is in the cookie, if anything
+    $http.defaults.headers.common['Authorization'] = 'Basic ' + $cookieStore.get('authdata');
+
+    return {
+        setCredentials: function (username, password) {
+            var encoded = Base64.encode(username + ':' + password);
+            $http.defaults.headers.common.Authorization = 'Basic ' + encoded;
+            $cookieStore.put('authdata', encoded);
+        },
+        clearCredentials: function () {
+            document.execCommand("ClearAuthenticationCache");
+            $cookieStore.remove('authdata');
+            $http.defaults.headers.common['Authorization'] = 'Basic ';
+        }
+    };
+})
+.run(function($rootScope, $location, $anchorScroll, $routeParams, $window, $cookies) {
+
+    $rootScope.$on('$routeChangeSuccess', function(newRoute, oldRoute) {
+            $location.hash($routeParams.scrollTo);
+            $anchorScroll();
+        },
+        $rootScope.location = $location
+    );
+});
--- spider/angular/controllers.js
+++ spider/angular/controllers.js
@@ -5,87 +5,180 @@
 
 angular.module('myApp.controllers', []).
 
-controller('LoginCtrl',function ($http, $scope, $location ,$window, $cookies) {
-    console.log('This is LoginCtrl');
-    $scope.login = function (user) {
+controller('LoginCtrl', function($http, $scope, $location, $window, $cookies) {
+    log('This is LoginCtrl');
+    $scope.login = function(user) {
 
     }
 
 }).
-controller('RegisterCtrl',function ($http, $scope, $location ,$window, $cookies) {
-    console.log('This is registerCtrl');
+controller('RegisterCtrl', function($http, $scope, $location, $window, $cookies) {
+    log('This is registerCtrl');
 
 }).
-controller('ForgotCtrl',function ($http, $scope, $location ,$window, $cookies) {
-    console.log('This is ForgotCtrl');
+controller('ForgotCtrl', function($http, $scope, $location, $window, $cookies) {
+    log('This is ForgotCtrl');
 
 }).
-controller('DashboardCtrl',function ($http, $scope, $location ,$window, $cookies) {
-    console.log('This is DashboardCtrl');
+controller('DashboardCtrl', function($http, $scope, $location, $window, $cookies) {
+    log('This is DashboardCtrl');
 
 }).
-controller('DocsCtrl',function ($http, $scope, $location ,$window, $cookies) {
-    console.log('This is DocsCtrl');
+controller('DocsCtrl', function($http, $scope, $location, $window, $cookies) {
+    log('This is DocsCtrl');
 
 }).
-controller('SettingCtrl',function ($http, $scope, $location ,$window, $cookies) {
-    console.log('This is SettingCtrl');
+controller('SettingCtrl', function($http, $scope, $location, $window, $cookies) {
+    log('This is SettingCtrl');
 
 }).
-controller('DocsCtrl',function ($http, $scope, $location ,$window, $cookies) {
-    console.log('This is DocsCtrl');
+controller('DocsCtrl', function($http, $scope, $location, $window, $cookies) {
+    log('This is DocsCtrl');
 
 }).
-controller('ScheduleCtrl',function ($http, $scope, $location ,$window, $cookies) {
-    console.log('This is ScheduleCtrl');
+controller('ScheduleCtrl', function($http, $scope, $location, $window, $cookies) {
+    log('This is ScheduleCtrl');
 
 }).
-controller('ProjectCtrl',function ($http, $scope, $location ,$window, $cookies,$routeParams) {
-    console.log('This is ProjectCtrl');
-    var current_project=$routeParams.current_project;
-    console.log(current_project);
-    $http.get('http://localhost:6800/listspiders.json?project='+current_project).then(function(response){
-        console.log(response)
-                //store to $scope.spiders
-                $scope.spiders=response.data['spiders']
-            }) 
-    $scope.start = function (spider) {
-        //var r='retailspiders'
-        //var a='adairboutique'
-        $http.post('http://localhost:6800/schedule.json',{project:'retailspiders',spider:'adairboutique'}).then(function(response) {
-            console.log(response)
-        })/*
-        $http({
-            method: 'POST',
-            url: 'http://localhost:6800/schedule.json',
-            data:{project:'retailspiders',spider:'adairboutique'},
-            headers: {'Content-Type': 'application/x-www-form-urlencoded'}
-        }).then(function(response) {
-            console.log(response)
-        })*/
-    }
+controller('PlanCtrl', function($http, $scope, $location, $window, $cookies) {
+    log('This is PlanCtrl');
+
 }).
-controller('TopNavCtrl', function ($scope,$http) {
-    $http.get('http://localhost:6800/listprojects.json').then(function (response) {
-        console.log(response)
-        $scope.projects=response.data['projects']
-        $scope.current_project =  $scope.projects[0]
-            //fetch spider of current
-            $http.get('http://localhost:6800/listspiders.json?project=retailspiders').then(function(response){
-                console.log(response)
-                //store to $scope.spiders
-                $scope.spiders=response.data['spiders']
-            }) 
-        })
-        //chageProject(project)
-        $scope.changeProject = function (project) {
-            //set project as current project
-            $scope.current_project=project
-            //load spiders of current project
-            $http.get('http://localhost:6800/listspiders.json?project='+project).then(function(response){
-                console.log(response)
-                //store to $scope.spiders
-                $scope.spiders=response.data['spiders']
-            }) 
+controller('PayCtrl', function($http, $scope, $location, $window, $cookies) {
+    log('This is PayCtrl');
+
+}).
+controller('ProjectCtrl', function(Auth,$http,$notification, $scope, $location, $window,$cookieStore, $cookies, $routeParams) {
+    var prjct
+    prjct=$cookies.project
+    console.log(prjct)
+    $scope.spiders=[]
+    $scope.name="Run"
+    $scope.data={}
+    Auth.setCredentials('manu','manu')
+    $http.get('http://localhost:6800/listspiders.json?project=' + prjct).then(function(response) {
+        //console.log(response.data)
+        //store to $scope.spiders
+        
+        for (var i=0;i<response.data['spiders'].length;i++) {
+            //console.log(response.data['spiders'][i])
+            $scope.spiders.push({
+                name:response.data['spiders'][i],
+                run:false,
+                lastJobid:null
+            })
         }
     })
+    $scope.$on("data", function() {
+        prjct=$cookies.project
+        $scope.spiders=[]
+        $http.get('http://localhost:6800/listspiders.json?project=' + prjct).then(function(response) {
+            console.log(response.data)
+        //store to $scope.spiders
+        
+        for (var i=0;i<response.data['spiders'].length;i++) {
+            //console.log(response.data['spiders'][i])
+            $scope.spiders.push({
+                name:response.data['spiders'][i],
+                run:false,
+                lastJobid:null
+            })
+        }
+    })
+    })
+    $scope.start = function(spider) {
+        $scope.selectedSpider=spider
+        $http.post('http://localhost:6800/schedule.json', $.param({
+            project: prjct,
+            spider: spider.name
+        })).then(function(response) {
+            console.log(response)
+            if(response.data.status=='ok'){
+                $notification.success('' +spider.name, 'Running with jobid' + response.data.jobid);
+                spider.run=true
+                spider.lastJobid=response.data.jobid
+            }
+            else{
+                $scope.n = notifyError()
+            }
+
+            console.log(spider)
+        })
+    }
+    $scope.stop = function(spider) {
+        console.log(prjct)
+        $http.post('http://localhost:6800/cancel.json', $.param({
+            project: prjct,
+            job: spider.lastJobid
+        })).then(function(response) {
+            console.log(response)
+            if(response.data.status=='ok'){
+                $notification.success('' +spider.name, 'Stoped');
+                spider.run=false
+            }
+            else{
+                $scope.n = notifyError()
+            }
+            
+        })
+    }
+
+}).
+controller('ProjectListCtrl', function($http, $scope, $location, MessageBus, $window,$cookieStore, $cookies,Auth) {
+    log('This is ProjectListCtrl');
+    Auth.setCredentials('manu','manu')
+    $http.get('http://localhost:6800/listprojects.json').then(function(response) {
+        log(response)
+        $scope.projects = response.data['projects']
+    })
+    $scope.proList = function(project) {
+        $cookies.project= project;
+        $location.path('/project');
+        MessageBus.broadcast("datapro");
+    }
+}).
+controller('TopNavCtrl', function($scope,$cookieStore,MessageBus,$cookies,$window,$location,$http,Auth) {
+    Auth.setCredentials('manu','manu')
+    var pro
+    $scope.$on("datapro", function() {
+        pro=$cookies.project
+        console.log(pro)
+        $http.get('http://localhost:6800/listprojects.json').then(function(response) {
+            log(response)
+            $scope.projects = response.data['projects']
+            $scope.current_project = pro
+        //fetch spider of current
+        $http.get('http://localhost:6800/listspiders.json?project='+$scope.current_project).then(function(response) {
+            log(response)
+            //store to $scope.spiders
+            $scope.spiders = response.data['spiders']
+        })
+    })
+    })
+    $http.get('http://localhost:6800/listprojects.json').then(function(response) {
+        log(response)
+        $scope.projects = response.data['projects']
+        $scope.current_project = $scope.projects[0]
+        //fetch spider of current
+        $http.get('http://localhost:6800/listspiders.json?project='+$scope.current_project).then(function(response) {
+            log(response)
+            //store to $scope.spiders
+            $scope.spiders = response.data['spiders']
+        })
+    })
+    //chageProject(project)
+    $scope.changeProject = function(project) {
+        
+        //set project as current project
+        $scope.current_project = project
+        $cookies.project= project;
+        //console.log($cookies.project)
+        MessageBus.broadcast("data");
+        //load spiders of current project
+        $http.get('http://localhost:6800/listspiders.json?project=' + $scope.current_project).then(function(response) {
+            log(response)
+            //store to $scope.spiders
+            $scope.spiders = response.data['spiders']
+        })
+    }
+})
